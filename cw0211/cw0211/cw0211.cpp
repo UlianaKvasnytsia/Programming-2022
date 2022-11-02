50ec1a7b9c99ec45bf2f311e502c7447b9a6add3
UlianaKvasnytsia <ulyana.kvasnytsya@gmail.com>
2022-11-02 15:47:09 +0200

    cout`s

--- cw0211/cw0211/cw0211.cpp
+++ cw0211/cw0211/cw0211.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 int main() {
 	int n, m;
+	cout << "enter the size of the first array: " << endl;
 	cin >> n;
 	int* a = new int[n];
+	cout << "enter the elements: " << endl;
 	for (int i = 0; i < n; i++)
 	{ cin >> a[i]; }
+	cout << "enter the size of the second array: " << endl;
 	cin >> m;
 	int* b = new int[m];
-	
+	cout << "enter the elements: " << endl;
 	for (int i = 0; i < m; i++) 
 	{ cin >> b[i]; }
 	int* result = new int[n+m];
@@ -31,6 +34,7 @@ int main() {
 			result[k] = b[j];
 			k++; j++;
 		}
+	cout << "sorted array: ";
 	for (k = 0; k < n+m; k++) {
 		cout << result[k]<< " ";
 	}
